70f3a4bb3609be621f29afbbe2f13052b757e409
2449149803@qq.com <>
2022-05-08 18:33:53 +0800

    feat: gitignore

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker run --name scrapy --rm -v $(pwd)/tutorial:/runtime/app aciobanu/scrapy scrapy crawl dyzz -o movie.json
+#!/bin/bash
+docker run --name scrapy --rm -v $(pwd)/tutorial:/runtime/app cai/scrapy scrapy crawl dyzz
+
